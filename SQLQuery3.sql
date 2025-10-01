e937f839593c6c504e2ecc212149ab8256d5477b
alen enyr <nerine032@gmail.com>
2025-10-01 08:53:57 +0800

    LE3

---
+++ SQLQuery3.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO [dbo].[Users] (UserName, FirstName, LastName, Password)
+VALUES 
+('user01', 'Amy', 'Winehouse', 'AmyPW1'),
+('user02', 'Britanny', 'Anderson', 'BritannyPW2'),
+('user03', 'Calvin', 'Bake', 'CalvinPW3');
